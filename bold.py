41bd37db2147b33dfb0fe66920a06b63c14921d7
pbircher <bircher.peter@mirango.hu>
2021-04-14 23:47:10 +0200

    init commit

---
+++ bold.py
@@ -0,0 +1,27 @@
+from docx import *
+
+document = Document('fold/sample.docx')
+bolds=[]
+italics=[]
+for para in document.paragraphs:
+    for run in para.runs:
+        print(run.text)
+        if run.italic :
+            italics.append(run.text)
+        if run.bold :
+            bolds.append(run.text)
+
+boltalic_Dict={'bold_phrases':bolds,
+              'italic_phrases':italics}
+              
+print(bolds)
+
+# from pdf2docx import Converter
+
+# pdf_file = 'fold/szerz.pdf'
+# docx_file = 'fold/sample.docx'
+
+# # convert pdf to docx
+# cv = Converter(pdf_file)
+# cv.convert(docx_file, start=0, end=None)
+# cv.close()
---
+++ valami.py
@@ -0,0 +1,157 @@
+import spacy
+import pandas as pd
+import numpy as np
+
+def checkSimpleSentence(sentence, nlp) -> bool:
+    hasFewVerbs = False
+    isUpperCaseOnly = False
+    isShort = False
+    isStartWithUpper = False
+    onlyNumber = True
+    isParagraphBefore = False
+    isParagraphAfter = False
+
+    doc = nlp(sentence)
+    verbs = 0
+    for token in doc:
+        if token.pos_ is 'VERB':
+            verbs += 1
+        if token.pos_ is not 'NUM':
+            onlyNumber = False
+
+    #     # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+    #     #     token.shape_, token.is_alpha, token.is_stop)
+
+    if verbs < 3:
+        hasFewVerbs = True
+
+    if not sentence:
+        return 'td'
+
+    if len(sentence) < 50 and len(sentence) > 1:
+        isShort = 1
+
+    # conditions
+    if onlyNumber or not isShort and not hasFewVerbs:
+        return 'p'
+
+    return 'uncertain'
+
+def checkSentence(doc, sentence, index, df, nlp) -> bool:
+    hasFewVerbs = False
+    isUpperCaseOnly = False
+    isShort = False
+    isStartWithUpper = False
+    onlyNumber = True
+    isParagraphBefore = False
+    isParagraphAfter = False
+
+    verbs = 0
+    for token in doc:
+        if token.pos_ is 'VERB':
+            verbs += 1
+        if token.pos_ is not 'NUM':
+            onlyNumber = False
+
+    #     # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+    #     #     token.shape_, token.is_alpha, token.is_stop)
+
+    if verbs < 3:
+        hasFewVerbs = True
+
+    if sentence:
+        if sentence.isupper() or sentence.istitle() or sentence[0].isupper():
+            isUpperCaseOnly = 1
+    else:
+        return 'td'
+
+    if index != 0 or len(df) is not index - 1:
+        if checkSimpleSentence(df['line'].iloc[index-1], nlp) is 'p':
+            isParagraphBefore = True
+        
+        if checkSimpleSentence(df['line'].iloc[index+1], nlp)  is 'p':
+            isParagraphAfter = True
+        
+    if len(sentence) < 50 and len(sentence) > 1:
+        isShort = 1
+
+    # conditions
+    if onlyNumber or not isShort and not hasFewVerbs or isParagraphBefore and not isParagraphAfter:
+        return 'p'
+
+    if isUpperCaseOnly and isShort and hasFewVerbs and isParagraphBefore and isParagraphAfter:
+        return 'title'
+
+    return 'uncertain'
+
+def main():
+    df = pd.read_csv('elso.csv')
+    df2 = pd.read_csv('masodik.csv')
+    df3 = pd.read_csv('harmadik.csv')
+    df4 = pd.read_csv('negyedik.csv')
+    df5 = pd.read_csv('otodik.csv')
+    df6 = pd.read_csv('hatodik.csv')
+    df7 = pd.read_csv('hetedik.csv')
+    df8 = pd.read_csv('nyolcadik.csv')
+    df9 = pd.read_csv('kilencedik.csv')
+    df10 = pd.read_csv('tizedik.csv')
+
+    # training_data = [df, df2, df3, df4, df5, df6, df7]
+    training_data = df2
+    test_data = pd.concat([df8, df9, df10])
+
+    training_data = training_data.replace(np.nan, '', regex=True)
+    test_data = test_data.replace(np.nan, '', regex=True)
+
+    nlp = spacy.load("en_core_web_sm")
+
+    # d = [df['line'].iloc[380],df['line'].iloc[103]] 
+
+    # for x in d:
+    #     doc = nlp(x)
+    #     for token in doc:
+    #         print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+    #             token.shape_, token.is_alpha, token.is_stop)
+
+    for index, row in training_data.iterrows():
+        print(index)
+        doc = nlp(row['line'])
+        row['pred'] = checkSentence(doc, row['line'], index, training_data, nlp)
+
+    training_data = training_data.drop(training_data[(training_data['pred'] == "uncertain")].index)
+    train = []
+    for index, row in training_data.iterrows():
+        cat = { "KAT": row['pred']}
+        train.append([row['line'], cat])
+
+    textcat=nlp.create_pipe( "textcat", config={"exclusive_classes": True, "architecture": "simple_cnn"})
+    nlp.add_pipe(text_cat, last=True)
+
+    # category = nlp.create_pipe("textcat")
+    # category.add_label("KAT")
+    # nlp.add_pipe(category)
+
+    # # Start the training
+    # nlp.begin_training()
+
+    # # Loop for 10 iterations
+    # for itn in range(100):
+    #     losses = {}
+
+    #     # Batch the examples and iterate over them
+    #     for batch in spacy.util.minibatch(train, size=1):
+    #         texts = [nlp(text) for text, entities in batch]
+    #         annotations = [{"cats": entities} for text, entities in batch]
+    #         nlp.update(texts, annotations, losses=losses)
+    #     if itn % 20 == 0:
+    #         print(losses)
+
+    # df.to_csv('masodik_eredmeny.csv', index = False)
+
+
+    #     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+    #             token.shape_, token.is_alpha, token.is_stop)
+
+
+if __name__ == "__main__":
+    main()

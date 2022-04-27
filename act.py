dfb2da4abf23313669a43579c4ad45bdfde151a1
unknown <birpet4@gmail.com>
2022-04-28 00:52:32 +0200

    fuckk

--- act.py
+++ act.py
@@ -311,7 +311,7 @@ def train_custom_ner():
             print('Losses', losses)
     nlp.max_length = 200000000
 
-    output_dir = "act_model"
+    output_dir = "act_model_v2"
     if output_dir is not None:
         output_dir = Path(output_dir)
         if not output_dir.exists():
@@ -330,13 +330,13 @@ def train_custom_ner():
     #     doc = nlp(all_of_it)
     #     displacy.serve(doc, style="ent")
 
-    # Test the trained model
-    test_text = """If the Issuer becomes subject to an insolvency proceeding and the Issuer has obligations to creditors that are treated under Irish law as creditors that are senior relative to the Noteholders and other Secured Parties, the Noteholders (and other Secured Parties) may suffer losses as a result of their subordinated status during such insolvency proceedings. In particular, under Irish law, upon an insolvency of an Irish company, such as the Issuer, when applying the proceeds of assets subject to fixed security which may have been realised in the course of a liquidation or receivership, the claims of a limited category of preferential creditors will take priority over the claims of creditors holding the relevant fixed security. These preferred claims include the remuneration, costs and expenses properly incurred by any examiner of the company (which may include any borrowings made by an examiner to fund the company’s requirements for the duration of his appointment) which have been approved by the relevant Irish courts. See 7.3 “Examinership”."""
-    # test_text = """The Issuer will depend upon the Asset Swap Counterparty to perform its obligations under any hedges. If the Asset Swap Counterparty defaults or becomes unable to perform due to insolvency or otherwise, the Issuer may not receive payments it would otherwise be entitled to from the Asset Swap Counterparty to cover its foreign exchange exposure."""
-    doc = nlp(test_text)
-    print("Entities")
-    for ent in doc.ents:
-        print("lab:", ent.label_, ent.text)
+    # # Test the trained model
+    # test_text = """If the Issuer becomes subject to an insolvency proceeding and the Issuer has obligations to creditors that are treated under Irish law as creditors that are senior relative to the Noteholders and other Secured Parties, the Noteholders (and other Secured Parties) may suffer losses as a result of their subordinated status during such insolvency proceedings. In particular, under Irish law, upon an insolvency of an Irish company, such as the Issuer, when applying the proceeds of assets subject to fixed security which may have been realised in the course of a liquidation or receivership, the claims of a limited category of preferential creditors will take priority over the claims of creditors holding the relevant fixed security. These preferred claims include the remuneration, costs and expenses properly incurred by any examiner of the company (which may include any borrowings made by an examiner to fund the company’s requirements for the duration of his appointment) which have been approved by the relevant Irish courts. See 7.3 “Examinership”."""
+    # # test_text = """The Issuer will depend upon the Asset Swap Counterparty to perform its obligations under any hedges. If the Asset Swap Counterparty defaults or becomes unable to perform due to insolvency or otherwise, the Issuer may not receive payments it would otherwise be entitled to from the Asset Swap Counterparty to cover its foreign exchange exposure."""
+    # doc = nlp(test_text)
+    # print("Entities")
+    # for ent in doc.ents:
+    #     print("lab:", ent.label_, ent.text)
 
 
 if __name__ == "__main__":
